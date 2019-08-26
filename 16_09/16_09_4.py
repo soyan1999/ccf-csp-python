2e881e20c82cd1cb27ba1950444da69b05c95212
soyan1999 <2973560852@qq.com>
2019-08-26 14:35:44 +0800

    upload

--- 16_09/16_09_4.py
+++ 16_09/16_09_4.py
@@ -2,28 +2,26 @@ from heapq import heapify, heappop, heappush
 from collections import defaultdict
 
 
-n = int(input())
-m = int(input())
-root = int(input()) - 1
+n, m = [int(item) for item in input().rstrip().split(' ')]
 road_set = defaultdict(list)
-max_length = -1
+all_length = 0
 
 for i in range(m):
-    src, dst, length = [int(item) for item in input().split(' ')]
+    src, dst, length = [int(item) for item in input().rstrip().split(' ')]
     road_set[src - 1].append((length, dst - 1))
     road_set[dst - 1].append((length, src - 1))
 
-in_set = {root}
-heap = road_set[root][:]
+in_set = {0}
+heap = [(item + (item[0],)) for item in road_set[0]]
 heapify(heap)
 
 while len(in_set) < n and len(heap) != 0:
-    length, dst = heappop(heap)
+    dis, dst, length = heappop(heap)
     if dst not in in_set:
         in_set.add(dst)
-        max_length = max(max_length, length)
+        all_length += length
         for ln, dt in road_set[dst]:
             if dt not in in_set:
-                heappush(heap, (ln, dt))
+                heappush(heap, (dis + ln, dt, ln))
 
-print(max_length)
+print(all_length)
